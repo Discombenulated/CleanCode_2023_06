8dbe08a63001de9e857326ba1a1fcc40e9353c51
Ben Noble <ben@benshouse.net>
2023-06-27 14:07:44 +0100

    Second test passes

--- chance.js
+++ chance.js
@@ -1,6 +1,10 @@
 class Chance{
-    equals(){
-        return true;
+    constructor(likelihood){
+        this._likelihood = likelihood;
+    }
+
+    equals(other){
+        return other._likelihood == this._likelihood;
     }
 }
 
--- chance.test.js
+++ chance.test.js
@@ -2,6 +2,10 @@ const {Chance} = require("./chance")
 
 describe('Chance...', () => {
     test('is equal to a chance with the same likelihood', () => {
-        expect(new Chance(1,6).equals(new Chance(1,6))).toBe(true);
+        expect(new Chance(0.5).equals(new Chance(0.5))).toBe(true);
+    })
+
+    test('is equal to a chance with the same likelihood', () => {
+        expect(new Chance(1).equals(new Chance(0))).not.toBe(true);
     })
 })
